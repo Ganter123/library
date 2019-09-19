d6e52967a2ffb99c2d687b2090c5244952336d9a
Ganter123 <49944342+Ganter123@users.noreply.github.com>
2019-09-19 15:44:04 +0530

    updated

--- vars/checkstyle.groovy
+++ vars/checkstyle.groovy
@@ -1,4 +1,7 @@
 #!/usr/bin/env groovy
+
 def call() {
- checkstyle canComputeNew: false, defaultEncoding: '', healthy: '', pattern: './node_modules/.bin/eslint -f checkstyle --ignore-path .gitignore . --fix > eslint.xml\'', unHealthy: ''
+
+ checkstyle canComputeNew: false, defaultEncoding: '', healthy: '', pattern: ' checkstyle pattern: \'eslint.xml\'', unHealthy: ''
+ 
 }
