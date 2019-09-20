56ba47b3a66ad21f9acde0aa7ea9ba47fca0c4ba
Ganter123 <49944342+Ganter123@users.noreply.github.com>
2019-09-20 15:19:57 +0530

    Update and rename email.groovy to _email.groovy

---
+++ vars/_email.groovy
@@ -0,0 +1,9 @@
+#!/usr/bin/env groovy
+
+def call(){
+
+emailext attachLog: true, body: '''Hello,
+your current build status is failed. please check the log here ${BUILD_URL}.  
+thanks''', compressLog: true, subject: 'build is failed. please check the log', to: 'darpan.patel@volansys.com'
+
+}
--- vars/email.groovy
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env groovy
-
-def call() {
-mail bcc: '', body: 'failed', cc: '', from: '', replyTo: '', subject: 'Build failed in Jenkins: $PROJECT_NAME - #$BUILD_NUMBER', to: 'darpan.patel@volansys.com' 
-}
- 
