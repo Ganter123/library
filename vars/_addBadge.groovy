67dba34616d4253f517af53d002a03acac4ff37d
Ganter123 <49944342+Ganter123@users.noreply.github.com>
2019-09-20 16:07:15 +0530

    Update _addBadge.groovy

--- vars/_addBadge.groovy
+++ vars/_addBadge.groovy
@@ -2,5 +2,5 @@
 
 def call() {
  addBadge icon: 'folder.gif', id: '', link: 'https://github.com/Ganter123/library.git', text: 'scm'
- addShortText background: '', borderColor: '', color: 'blue', link: 'https://github.com/Ganter123/library.git', text: '${GIT_AUTHOR_NAME}'
+ addShortText background: '', borderColor: '', color: 'blue', link: 'https://github.com/Ganter123/library.git', text: '${Ganter123}'
 }
