e7f3099b0e1cf575d41c529cac531aaca7eda3d3
Ganter123 <49944342+Ganter123@users.noreply.github.com>
2019-10-10 15:38:44 +0530

    Update _apijen.groovy

--- vars/_apijen.groovy
+++ vars/_apijen.groovy
@@ -31,13 +31,14 @@ def call(){
      } 
      
      stage ('flocknotity') {
-     steps {
-      
-       FLOCK_MESSAGE = """<flockml>
+     steps { 
+         '''
+        FLOCK_MESSAGE = """<flockml>
         Branch <b><a href='${URL_BRANCH}'>${DEPLOYED_BRANCH}</a></b> has been deployed<br/>
         MSS:         <b><a href='${URL_JENKINS}'>${DEPLOYED_MSS}</a></b><br/>
         Environment: <b>${ENV}</b><br/>
         </flockml>""".stripIndent()
+                 '''
      }
      }
      
